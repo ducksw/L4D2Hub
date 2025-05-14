871a41c4f596cd8bf39605ecf920b1207ae65e80
Ducks <cansgamer@gmail.com>
2025-05-14 01:44:27 -0500

    match 1

--- client/match.js
+++ client/match.js
@@ -1,26 +1,26 @@
 import { players } from '../models/player.js'
 
 function getBarClass(index) {
-  if (index < 4) return 'bar-blue';
-  //if (index < 8) return 'bar-orange';
-  //if (index < 9) return 'bar-green';
+    if (index < 4) return 'bar-blue';
+    //if (index < 8) return 'bar-orange';
+    //if (index < 9) return 'bar-green';
 }
 
 function stats() {
-  let res = document.getElementById('liga');
-  let rec = `<table>
-    <tr>
-      <th>#</th>
-      <th>Name:</th>
-      <th>Profile:</th>
-      <th>Steam Id:</th>
-      <th>Puntos:</th>
-      <th>Ultimo 5</th>
-    </tr>
+    let res = document.getElementById('liga');
+    let rec = `<table>
+        <tr>
+          <th>#</th>
+          <th>Name:</th>
+          <th>Profile:</th>
+          <th>Steam Id:</th>
+          <th>Puntos:</th>
+          <th>Ultimo 5</th>
+        </tr>
     `
-  players.sort((a, b) => (b.points) - (a.points));
+    players.sort((a, b) => (b.points) - (a.points));
 
-  players.forEach((player, index) => {
+    players.forEach((player, index) => {
     /*
     const wins = parseInt(player.win) || 0;
     const losses = parseInt(player.losser) || 0;
@@ -32,31 +32,31 @@ function stats() {
 
     // Mostrar últimos 5 como íconos
     const last = (player.lastMatches || []).map(r => {
-      let className = ""
-      if (r === 'W') className = 'dot dot-win';
-      if (r === 'L') className = 'dot dot-loss';
-      if (r === 'D') className = 'dot dot-draw';
-      if (r === 'N') className = 'dot dot-none';
-      return `<div class="${className}"></div>`;
+        let className = ""
+        if (r === 'W') className = 'dot dot-win';
+        if (r === 'L') className = 'dot dot-loss';
+        if (r === 'D') className = 'dot dot-draw';
+        if (r === 'N') className = 'dot dot-none';
+        return `<div class="${className}"></div>`;
     }).join('');
     rec +=`
     <tr>
-      <td class="${getBarClass(index)}"><b>${index + 1}</b></td>
-      <td>${player.name}</td>
-      <td class="avatar"><a target="_blank" href="${player.profileurl}"><img src="${player.avatar}"/></a></td>
-      <td class="steamid" style="color: darkorange;">${player.steamId}</td>
-      <td class="elo">${player.points}</td>
-      <td class="last-matches">${last}</td>
+        <td class="${getBarClass(index)}"><b>${index + 1}</b></td>
+        <td>${player.name}</td>
+        <td class="avatar"><a target="_blank" href="${player.profileurl}"><img src="${player.avatar}"/></a></td>
+        <td class="steamid" style="color: darkorange;">${player.steamId}</td>
+        <td class="elo">${player.points}</td>
+        <td class="last-matches">${last}</td>
     </tr>`;
-  });
-  rec += "</table>";
+    });
+    rec += "</table>";
 
-  res.innerHTML = rec;
+    res.innerHTML = rec;
 }
 
 document.getElementById('ch').addEventListener('click', function() {
-  alert("Primero, se deben clasificar los jugadores para que pueda comenzar la Champions Bulls.");
-  preventDefault();
+    alert("Primero, se deben clasificar los jugadores para que pueda comenzar la Champions Bulls.");
+    preventDefault();
 })
 
 stats();
--- client/stats.js
+++ client/stats.js
@@ -1,200 +1,200 @@
 import { players } from '../models/player.js'
 
 function stats() {
-  let res = document.getElementById('res');
-  let rec = `<table>
-    <tr>
-      <th>#</th>
-      <th>Name:</th>
-      <th>Profile:</th>
-      <th>Steam Id:</th>
-      <th>Elo:</th>
-      <th>Damage:</th>
-      <th>Kills:</th>
-      <th>Win:</th>
-      <th>Loser:</th>
-      <th>Empate:</th>
-      <th>Matchs:</th>
-      <th>Rank:</th>
-    </tr>
+    let res = document.getElementById('res');
+    let rec = `<table>
+        <tr>
+            <th>#</th>
+            <th>Name:</th>
+            <th>Profile:</th>
+            <th>Steam Id:</th>
+            <th>Elo:</th>
+            <th>Damage:</th>
+            <th>Kills:</th>
+            <th>Win:</th>
+            <th>Loser:</th>
+            <th>Empate:</th>
+            <th>Matchs:</th>
+            <th>Rank:</th>
+        </tr>
     `
 
-  players.sort((a, b) => (b.elo) - (a.elo));
+    players.sort((a, b) => (b.elo) - (a.elo));
 
-  players.forEach((player, index) => {
+    players.forEach((player, index) => {
     rec +=`
     <tr>
-      <td><b>${index + 1}</b></td>
-      <td>${player.name}</td>
-      <td class="avatar"><a target="_blank" href="${player.profileurl}"><img src="${player.avatar}"/></a></td>
-      <td class="steamid" style="color: darkorange;">${player.steamId}</td>
-      <td class="elo">${player.elo}</td>
-      <td>${player.damage}</td>
-      <td>${player.kills}</td>
-      <td>${player.win}</td>
-      <td>${player.losser}</td>
-      <td>${player.draw}</td>
-      <td>${player.match}</td>
-      <td class="rank-color"><img class="rank-image" src="image/brozen.svg" style="max-width: 100%; width: 50px;"></td>
+        <td><b>${index + 1}</b></td>
+        <td>${player.name}</td>
+        <td class="avatar"><a target="_blank" href="${player.profileurl}"><img src="${player.avatar}"/></a></td>
+        <td class="steamid" style="color: darkorange;">${player.steamId}</td>
+        <td class="elo">${player.elo}</td>
+        <td>${player.damage}</td>
+        <td>${player.kills}</td>
+        <td>${player.win} 🏆</td>
+        <td>${player.losser}</td>
+        <td>${player.draw}</td>
+        <td>${player.match}</td>
+        <td class="rank-color"><img class="rank-image" src="image/brozen.svg" style="max-width: 100%; width: 50px;"></td>
     </tr>`;
-  });
-  rec += "</table>";
+    });
+    rec += "</table>";
 
-  res.innerHTML = rec;
+    res.innerHTML = rec;
 
-  const rankElements = document.querySelectorAll('.rank-image');
-  rankElements.forEach((el, i) => {
+    const rankElements = document.querySelectorAll('.rank-image');
+    rankElements.forEach((el, i) => {
     const rank = players[i].rank;
-    if (rank === "Brozen") {
-      //el.src = "image/bronze.svg"
-    } else if (rank === "Silver") {
-      el.src = "image/silver.svg"
-      // el.classList.add('silver');
-    } else if (rank === "Gold") {
-      el.src = "image/gold.svg"
-    } else if (rank === "Platinum") {
-      el.src = "image/platinum.svg"
-    } else if (rank === "Diamond") {
-      el.src = "image/diamond.svg"
-    } else if (rank === "Champions") {
-      el.src = "image/champions.svg"
-    }
-  });
+        if (rank === "Brozen") {
+            //el.src = "image/bronze.svg"
+        } else if (rank === "Silver") {
+            el.src = "image/silver.svg"
+            // el.classList.add('silver');
+        } else if (rank === "Gold") {
+            el.src = "image/gold.svg"
+        } else if (rank === "Platinum") {
+            el.src = "image/platinum.svg"
+        } else if (rank === "Diamond") {
+            el.src = "image/diamond.svg"
+        } else if (rank === "Champions") {
+            el.src = "image/champions.svg"
+        }
+    });
 }
 
 function rankingElo() {
-  let table_elo = document.getElementById('table-elo');
+    let table_elo = document.getElementById('table-elo');
 
-  players.sort((a, b) => (b.elo) - (a.elo));
-  let rec = `<table class="table">
-    <tr>
-      <th class="th">#</th>
-      <th class="th">Name:</th>
-      <th class="th">Profile:</th>
-      <th class="th">Elo:</th>
-    </tr>
+    players.sort((a, b) => (b.elo) - (a.elo));
+    let rec = `<table class="table">
+        <tr>
+          <th class="th">#</th>
+          <th class="th">Name:</th>
+          <th class="th">Profile:</th>
+          <th class="th">Elo:</th>
+        </tr>
     `
-  let topPlayers  = players.slice(0,5);
-  topPlayers.forEach((player, index) => {
+    let topPlayers  = players.slice(0,5);
+    topPlayers.forEach((player, index) => {
     rec +=`
-    <tr>
-      <td class="td"><b>${index + 1}</b></td>
-      <td class="td">${player.name}</td>
-      <td class="avatar td"><img src="${player.avatar}"/></td>
-      <td class="elo td">${player.elo}</td>
-    </tr>`;
-  });
-  rec += "</table>";
+        <tr>
+            <td class="td"><b>${index + 1}</b></td>
+            <td class="td">${player.name}</td>
+            <td class="avatar td"><img src="${player.avatar}"/></td>
+            <td class="elo td">${player.elo}</td>
+        </tr>`;
+    });
+    rec += "</table>";
 
-  table_elo.innerHTML = rec;
+    table_elo.innerHTML = rec;
 }
 
 function rankingDamage() {
-  let table_damage = document.getElementById('table-damage');
+    let table_damage = document.getElementById('table-damage');
 
-  players.sort((a, b) => (b.damage) - (a.damage));
-  let rec = `<table class="table">
-    <tr>
-      <th class="th">#</th>
-      <th class="th">Name:</th>
-      <th class="th">Profile:</th>
-      <th class="th">Damage:</th>
-    </tr>
+    players.sort((a, b) => (b.damage) - (a.damage));
+    let rec = `<table class="table">
+        <tr>
+            <th class="th">#</th>
+            <th class="th">Name:</th>
+            <th class="th">Profile:</th>
+            <th class="th">Damage:</th>
+        </tr>
     `
-  let topPlayers  = players.slice(0,5);
-  topPlayers.forEach((player, index) => {
+    let topPlayers  = players.slice(0,5);
+    topPlayers.forEach((player, index) => {
     rec +=`
-    <tr>
-      <td class="td"><b>${index + 1}</b></td>
-      <td class="td">${player.name}</td>
-      <td class="avatar td"><img src="${player.avatar}"/></td>
-      <td class="elo td">${player.damage}</td>
-    </tr>`;
-  });
-  rec += "</table>";
+        <tr>
+            <td class="td"><b>${index + 1}</b></td>
+            <td class="td">${player.name}</td>
+            <td class="avatar td"><img src="${player.avatar}"/></td>
+            <td class="elo td">${player.damage}</td>
+        </tr>`;
+    });
+    rec += "</table>";
 
-  table_damage.innerHTML = rec;
+    table_damage.innerHTML = rec;
 }
 
 function rankingKill() {
-  let table_kill = document.getElementById('table-kill');
+    let table_kill = document.getElementById('table-kill');
 
-  players.sort((a, b) => (b.kills) - (a.kills));
-  let rec = `<table class="table">
+    players.sort((a, b) => (b.kills) - (a.kills));
+    let rec = `<table class="table">
     <tr>
-      <th class="th">#</th>
-      <th class="th">Name:</th>
-      <th class="th">Profile:</th>
-      <th class="th">Kills:</th>
+        <th class="th">#</th>
+        <th class="th">Name:</th>
+        <th class="th">Profile:</th>
+        <th class="th">Kills:</th>
     </tr>
     `
-  let topPlayers  = players.slice(0,5);
-  topPlayers.forEach((player, index) => {
+    let topPlayers  = players.slice(0,5);
+    topPlayers.forEach((player, index) => {
     rec +=`
     <tr>
-      <td class="td"><b>${index + 1}</b></td>
-      <td class="td">${player.name}</td>
-      <td class="avatar td"><img src="${player.avatar}"/></td>
-      <td class="elo td">${player.kills}</td>
+        <td class="td"><b>${index + 1}</b></td>
+        <td class="td">${player.name}</td>
+        <td class="avatar td"><img src="${player.avatar}"/></td>
+        <td class="elo td">${player.kills}</td>
     </tr>`;
-  });
-  rec += "</table>";
+    });
+    rec += "</table>";
 
-  table_kill.innerHTML = rec;
+    table_kill.innerHTML = rec;
 }
 
 function rankingWin() {
-  let table_win = document.getElementById('table-win');
+    let table_win = document.getElementById('table-win');
 
-  players.sort((a, b) => (b.win) - (a.win));
-  let rec = `<table class="table">
+    players.sort((a, b) => (b.win) - (a.win));
+    let rec = `<table class="table">
     <tr>
-      <th class="th">#</th>
-      <th class="th">Name:</th>
-      <th class="th">Profile:</th>
-      <th class="th">Wins:</th>
+        <th class="th">#</th>
+        <th class="th">Name:</th>
+        <th class="th">Profile:</th>
+        <th class="th">Wins:</th>
     </tr>
     `
-  let topPlayers  = players.slice(0,5);
-  topPlayers.forEach((player, index) => {
+    let topPlayers  = players.slice(0,5);
+    topPlayers.forEach((player, index) => {
     rec +=`
     <tr>
-      <td class="td"><b>${index + 1}</b></td>
-      <td class="td">${player.name}</td>
-      <td class="avatar td"><img src="${player.avatar}"/></td>
-      <td class="elo td">${player.win} 🏆</td>
+        <td class="td"><b>${index + 1}</b></td>
+        <td class="td">${player.name}</td>
+        <td class="avatar td"><img src="${player.avatar}"/></td>
+        <td class="elo td">${player.win} 🏆</td>
     </tr>`;
-  });
-  rec += "</table>";
+    });
+    rec += "</table>";
 
-  table_win.innerHTML = rec;
+    table_win.innerHTML = rec;
 }
 
 function rankingLoser() {
-  let table_loser = document.getElementById('table-loser');
+    let table_loser = document.getElementById('table-loser');
 
-  players.sort((a, b) => (b.losser) - (a.losser));
-  let rec = `<table class="table">
+    players.sort((a, b) => (b.losser) - (a.losser));
+    let rec = `<table class="table">
     <tr>
-      <th class="th">#</th>
-      <th class="th">Name:</th>
-      <th class="th">Profile:</th>
-      <th class="th">Loser:</th>
+        <th class="th">#</th>
+        <th class="th">Name:</th>
+        <th class="th">Profile:</th>
+        <th class="th">Loser:</th>
     </tr>
     `
-  let topPlayers  = players.slice(0,5);
-  topPlayers.forEach((player, index) => {
-    rec +=`
-    <tr>
-      <td class="td"><b>${index + 1}</b></td>
-      <td class="td">${player.name}</td>
-      <td class="avatar td"><img src="${player.avatar}"/></td>
-      <td class="elo td">${player.losser}</td>
-    </tr>`;
-  });
-  rec += "</table>";
+    let topPlayers  = players.slice(0,5);
+    topPlayers.forEach((player, index) => {
+        rec +=`
+        <tr>
+            <td class="td"><b>${index + 1}</b></td>
+            <td class="td">${player.name}</td>
+            <td class="avatar td"><img src="${player.avatar}"/></td>
+            <td class="elo td">${player.losser}</td>
+        </tr>`;
+    });
+    rec += "</table>";
 
-  table_loser.innerHTML = rec;
+    table_loser.innerHTML = rec;
 }
 
 rankingLoser();
