dd272bc43530a1750346e0a660ae512328d2316b
Ducks <cansgamer@gmail.com>
2025-05-21 17:34:50 -0500

    fixed image

--- models/posts.js
+++ models/posts.js
@@ -3,7 +3,7 @@ export const posts = [
   {
     title: "Y LO VOLVIO HACER!!",
     parr: "La triste derrota de un equipo que merecia mas que eso bajoneo un equipo entero pedro el chico no sabe que esta compitiendo en una liga?? los comentaristas se preguntan si acaso el chico sabe que esta jugando con gente de tal caracter para poder equilibrarze? que hace jugando en un mal momento de internet que son las excusas oh las palabras clave que nos muestra nuestro jugador pedro ojala se recupere de esta derrota y esperemos sea la ultima que hoy deja sin chances a 2 mas de sus compañeros aun falta mucho por mas saludos!!! -Premier Legue Bulls",
-    image: "../image/c-pedro02.jpeg",
+    image: "image/c-pedro02.jpeg",
     fecha: "20/05/2025",
   },
 
@@ -11,7 +11,7 @@ export const posts = [
   {
     title: "La cara de la felicidad y la otra cara de la tristeza",
     parr: "Lo que se les escapo por unos puntos! Un resbalon de pedro fue lo mas decepcionante del inicio de la jornada 1 dejando atras a todo un equipo que batallo imnumerables veces por llegar a la victoria deja la delantera por esta vez Podra En los siguientes encuentros lograr la gloria por la cima? Recordemos este es el inicio los comentaristas hablan del gran duelo que se vivio hoy el encuentro es unico un equipo tan cerca y tan lejos oh la mediocridad los consumira 1000 horas para pedro no son suficiente Esperamos Mas en los siguientes encuentros.",
-    image: "../image/c-pedro.jpeg",
+    image: "image/c-pedro.jpeg",
     fecha: "14/05/2025",
   }
 ]
